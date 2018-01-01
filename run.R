d498fb3b48db8f6f343dfae2b423da4c603b739a
Clint Modien <cmodien@gmail.com>
2018-01-01 13:50:54 -0600

    Moving app to app.R and adding proper run.R for heroku

--- run.R
+++ run.R
@@ -1,27 +1,9 @@
-library(curl)
-library(jsonlite)
-library(rpivotTable)
 library(shiny)
-library(shinydashboard)
 
-# https://salty-dusk-41217.herokuapp.com/siliconpublishing
-shinyApp(
-  shinyUI(
-    dashboardPage(
-      dashboardHeader(title = "Github Stats", titleWidth = "100%"),
-      dashboardSidebar(disable = T),
-      dashboardBody(
-        tags$head(tags$style( type = 'text/css',  '#pivot{ overflow-x: scroll; }')),
-        rpivotTableOutput('pivot', width = "100%", height = "100%")
-      )
-    )
-  ),
-  shinyServer(function(input, output, session) {
-    df = fromJSON('https://salty-dusk-41217.herokuapp.com/siliconpublishing')
-    output$pivot <- renderRpivotTable({
-      rpivotTable(data = df, rows = "Year",
-                  cols = "Month", rendererName = "Row Heatmap",
-                  aggregatorName = "Integer Sum", vals = "Commits")
-    })
-  })
+port <- Sys.getenv('PORT')
+
+shiny::runApp(
+  appDir = getwd(),
+  host = '0.0.0.0',
+  port = as.numeric(port)
 )
