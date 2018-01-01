d8131973f003c5b8f1ac8d43b43e88074b70f7a2
Clint Modien <cmodien@gmail.com>
2018-01-01 16:13:01 -0600

    trying to fix H15 socket timeout on heroku

--- app.R
+++ app.R
@@ -5,23 +5,42 @@ library(shiny)
 library(shinydashboard)
 
 # https://salty-dusk-41217.herokuapp.com/siliconpublishing
-shinyApp(
-  shinyUI(
-    dashboardPage(
-      dashboardHeader(title = "Github Stats", titleWidth = "100%"),
-      dashboardSidebar(disable = T),
-      dashboardBody(
-        tags$head(tags$style( type = 'text/css',  '#pivot{ overflow-x: scroll; }')),
-        rpivotTableOutput('pivot', width = "100%", height = "100%")
-      )
+
+shinyApp(fluidPage(
+  tags$head(
+    HTML(
+      "
+      <script>
+      $(document).on('shiny:connected', function(event) {
+        setTimeout(function(){
+          event.socket.emit('ping')
+        }, 1000)
+      });
+      </script>
+      "
     )
+    ),
+  dashboardPage(
+    dashboardHeader(title = "Github Stats", titleWidth = "100%"),
+    dashboardSidebar(disable = T),
+    dashboardBody(
+      tags$head(tags$style(type = 'text/css',  '#pivot{ overflow-x: scroll; }')),
+      rpivotTableOutput('pivot', width = "100%", height = "100%")
+    )
+  )
   ),
   shinyServer(function(input, output, session) {
-    df = fromJSON('https://salty-dusk-41217.herokuapp.com/siliconpublishing')
+    df = fromJSON(
+      'https://salty-dusk-41217.herokuapp.com/siliconpublishing'
+    )
     output$pivot <- renderRpivotTable({
-      rpivotTable(data = df, rows = "Year",
-                  cols = "Month", rendererName = "Row Heatmap",
-                  aggregatorName = "Integer Sum", vals = "Commits")
+      rpivotTable(
+        data = df,
+        rows = "Year",
+        cols = "Month",
+        rendererName = "Row Heatmap",
+        aggregatorName = "Integer Sum",
+        vals = "Commits"
+      )
     })
-  })
-)
+  }))
